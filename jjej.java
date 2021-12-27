71eb6ce3e2713de15edf5eb599519ebd4cf86bc3
LutchetConcede <96726632+LutchetConcede@users.noreply.github.com>
2021-12-27 15:40:09 +0100

    Update jjej.java

--- jjej.java
+++ jjej.java
@@ -1,4 +1,40 @@
-URL url = new URL("https://vivado.requestcatcher.com");
-HttpURLConnection con = (HttpURLConnection) url.openConnection();
-con.setRequestMethod("GET");
-OutputStream os = con.getOutputStream();
+public static String executePost() {
+  HttpURLConnection connection = null;
+
+  try {
+    //Create connection
+    URL url = new URL("https://vivado.requestcatcher.com");
+    connection = (HttpURLConnection) url.openConnection();
+    connection.setRequestMethod("POST");
+    connection.setRequestProperty("Content-Type", 
+        "application/x-www-form-urlencoded");
+
+    connection.setUseCaches(false);
+    connection.setDoOutput(true);
+
+    //Send request
+    DataOutputStream wr = new DataOutputStream (
+        connection.getOutputStream());
+    wr.writeBytes(urlParameters);
+    wr.close();
+
+    //Get Response  
+    InputStream is = connection.getInputStream();
+    BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+    StringBuilder response = new StringBuilder(); // or StringBuffer if Java version 5+
+    String line;
+    while ((line = rd.readLine()) != null) {
+      response.append(line);
+      response.append('\r');
+    }
+    rd.close();
+    return response.toString();
+  } catch (Exception e) {
+    e.printStackTrace();
+    return null;
+  } finally {
+    if (connection != null) {
+      connection.disconnect();
+    }
+  }
+}
