25e6f7725c34d593b2a0fa4bde21005871166179
LutchetConcede <96726632+LutchetConcede@users.noreply.github.com>
2021-12-27 15:37:16 +0100

    Update jjej.java

--- jjej.java
+++ jjej.java
@@ -1,4 +1,4 @@
-URL url = new URL("http://example.com");
+URL url = new URL("https://vivado.requestcatcher.com");
 HttpURLConnection con = (HttpURLConnection) url.openConnection();
 con.setRequestMethod("GET");
 OutputStream os = con.getOutputStream();
